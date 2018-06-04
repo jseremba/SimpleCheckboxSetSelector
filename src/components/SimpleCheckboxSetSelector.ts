d688e5992fa0bee3a35d5bf068332cf39ed2b7b0
Joseph D <j_seremba@live.com>
2018-06-04 08:50:59 +0300

    Add checkbox implementation

--- src/components/SimpleCheckboxSetSelector.ts
+++ src/components/SimpleCheckboxSetSelector.ts
@@ -2,47 +2,85 @@ import { Component, createElement, ChangeEvent } from "react";
 import * as classNames from "classnames";
 
 interface SimpleCheckboxSetSelectorProps {
-    caption: string;
-    isChecked?: boolean;
-    defaultChecked?: boolean;
-    handleChange?: (value: boolean) => void;
+    alertMessage: string;
+    labelCaption: string;
+    checkboxItems: CheckboxOptions[];
+    direction: Direction;
+    handleChange: (value: boolean, guid: string) => void;
     readOnly?: boolean;
-    checkboxItems?: CheckboxOptions[];
+}
+
+interface SimpleCheckboxSetSelectorState {
+    checkboxItems: CheckboxOptions[];
+    labelCaption: string;
 }
 
 export interface CheckboxOptions {
     guid: string;
-    value: string | number | boolean;
-    checked: boolean;
+    caption: string | number | boolean;
+    isChecked: boolean;
 }
 
-export class SimpleCheckboxSetSelector extends Component<SimpleCheckboxSetSelectorProps, { isChecked: boolean }> {
+export type Direction = "horizontal" | "vertical";
+export type SortOrder = "asc" | "desc";
+
+export class SimpleCheckboxSetSelector extends Component<SimpleCheckboxSetSelectorProps, SimpleCheckboxSetSelectorState> {
 
     constructor(props: SimpleCheckboxSetSelectorProps) {
         super(props);
 
-        this.state = { isChecked : this.props.isChecked || false };
+        this.state = {
+            checkboxItems : this.props.checkboxItems,
+            labelCaption: this.props.labelCaption
+        };
     }
 
     render() {
-        return createElement("div", { className: classNames("form-group checkboxMainContainer") },
-            createElement("label", { className: classNames("control-label checkboxLabel") }, this.props.caption),
-            createElement("div", {}, this.createCheckboxNodes()),
+        return createElement("div", { className: classNames("form-group widget-checkbox-set-selector") },
+            createElement("label", { className: classNames("control-label checkboxLabel") }, this.state.labelCaption),
+            this.createCheckboxNodes(),
             createElement("div", { className: classNames("showmoreButton") })
         );
     }
 
+    componentWillReceiveProps(newProps: SimpleCheckboxSetSelectorProps) {
+        if (this.state.checkboxItems !== newProps.checkboxItems) {
+            this.setState({
+                checkboxItems : newProps.checkboxItems,
+                labelCaption: newProps.labelCaption
+            });
+        }
+    }
+
     private createCheckboxNodes = () => {
-        return createElement("input", {
-            checked: this.props.isChecked,
-            defaultChecked: this.props.defaultChecked,
-            onChange: this.handleOnChange,
-            type: "checkbox"
-        });
+        const { checkboxItems } = this.state;
+        if (checkboxItems && checkboxItems.length > 0) {
+            return checkboxItems.map(checkbox => {
+                if (this.props.direction === "horizontal") {
+                    return this.checkboxLabel(checkbox.caption, checkbox.isChecked, "checkbox-inline", checkbox.guid);
+                } else {
+                    return createElement("div", { className: classNames("checkbox") },
+                        this.checkboxLabel(checkbox.caption, checkbox.isChecked, "", checkbox.guid));
+                }
+            });
+        }
+        return [];
+    }
+
+    private checkboxLabel = (caption: string | number | boolean, checked: boolean, className: string, guid: string) => {
+        return createElement("label", { className },
+            createElement("input", {
+                checked,
+                disabled: this.props.readOnly,
+                value: guid,
+                onChange: this.handleOnChange,
+                type: "checkbox"
+            }),
+            createElement("span", {}, caption)
+        )
     }
 
     private handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-        this.setState({ isChecked: event.target.checked });
-        this.props.handleChange(event.target.checked);
+        this.props.handleChange(event.target.checked, event.target.value);
     }
 }
--- src/components/SimpleCheckboxSetSelectorContainer.ts
+++ src/components/SimpleCheckboxSetSelectorContainer.ts
@@ -1,9 +1,198 @@
 import { Component, createElement } from "react";
 
-import { SimpleCheckboxSetSelector } from "./SimpleCheckboxSetSelector";
+import { SimpleCheckboxSetSelector, CheckboxOptions, Direction, SortOrder } from "./SimpleCheckboxSetSelector";
 
-export default class SimpleCheckboxSetSelectorContainer extends Component<{}, {}> {
+interface WrapperProps {
+    "class": string;
+    mxform: mxui.lib.form._FormBase;
+    mxObject: mendix.lib.MxObject;
+    style: string;
+    readOnly: boolean;
+}
+
+interface SimpleCheckboxSetSelectorContainerProps extends WrapperProps {
+    dataSourceType: "xpath" | "microflow";
+    associationReference: string;
+    dataSourceMicroflow: string;
+    constraint: string;
+    showLabel: string;
+    labelCaption: string;
+    sortAttribute: string;
+    sortOrder: SortOrder;
+    labelAttribute: string;
+    formOrientation: string;
+    direction: Direction;
+    labelWidth: string;
+    showMore: string;
+    editable: "default" | "never";
+    onChangeMicroflow: string;
+    onChangeDelay: string;
+}
+
+interface SimpleCheckboxSetSelectorContainerState {
+    checkboxItems: CheckboxOptions[];
+    isChecked: boolean;
+    alertMessage: string;
+}
+
+export default class SimpleCheckboxSetSelectorContainer extends Component<SimpleCheckboxSetSelectorContainerProps, SimpleCheckboxSetSelectorContainerState> {
+    private entity: string;
+    private reference: string;
+    private subscriptionHandles: number[] = [];
+
+    constructor(props: SimpleCheckboxSetSelectorContainerProps) {
+        super(props);
+
+        this.state = {
+            alertMessage: "",
+            checkboxItems: [],
+            isChecked: false
+        };
+
+        this.entity = this.props.associationReference.split("/")[1];
+        this.reference = this.props.associationReference.split("/")[0];
+    }
     render() {
-        return createElement(SimpleCheckboxSetSelector);
+        return createElement(SimpleCheckboxSetSelector, {
+            alertMessage: this.state.alertMessage,
+            labelCaption: this.props.labelCaption,
+            checkboxItems: this.state.checkboxItems,
+            direction: this.props.direction,
+            handleChange: this.handleChange,
+            readOnly: this.isReadOnly()
+        });
+    }
+
+    componentDidMount() {
+        this.fetchData(this.props.mxObject);
+    }
+
+    componentWillReceiveProps(nextProps: SimpleCheckboxSetSelectorContainerProps) {
+        if (nextProps.mxObject !== this.props.mxObject) {
+            this.fetchData(nextProps.mxObject);
+            this.resetSubscriptions(nextProps.mxObject);
+        }
+    }
+
+    componentWillUnmount() {
+        this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+    }
+
+    private isReadOnly = (): boolean => {
+        return !this.props.mxObject || this.props.editable === "never" ||
+            this.props.mxObject.isReadonlyAttr(this.reference)
+    }
+
+    private resetSubscriptions = (mxObject?: mendix.lib.MxObject) => {
+        this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+        this.subscriptionHandles = [];
+
+        if (mxObject) {
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                guid: mxObject.getGuid(),
+                val: true,
+                callback: () => this.handleValidations
+            }));
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                guid: mxObject.getGuid(),
+                callback: () => this.fetchData(mxObject)
+            }));
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                guid: mxObject.getGuid(),
+                attr: this.reference,
+                callback:() => this.fetchData(mxObject)
+            }));
+        }
+    }
+
+    private handleValidations = (validations: mendix.lib.ObjectValidation[]) => {
+        const alertMessage = validations[0].getErrorReason(this.reference);
+        validations[0].removeAttribute(this.reference);
+        if (alertMessage) {
+            this.setState({ alertMessage });
+        }
+    }
+
+    private handleChange = (value: boolean, guid: string) => {
+        const { mxObject } = this.props;
+        if (value && guid) {
+            mxObject.addReference(this.reference, guid);
+        } else {
+            mxObject.removeReferences(this.reference, [guid]);
+        }
+    }
+
+    private fetchData(contextObject?: mendix.lib.MxObject) {
+        const { dataSourceType, dataSourceMicroflow } = this.props;
+
+        if (contextObject) {
+            if (dataSourceType === "xpath") {
+                this.getDataFromXPath(contextObject.getGuid());
+            } else if (dataSourceType === "microflow" && dataSourceMicroflow) {
+                this.getDataFromMicroflow(contextObject);
+            }
+        }
+    }
+
+    private getDataFromXPath = (guid: string) => {
+        const constraint = this.props.constraint
+            ? this.props.constraint.replace(/\[\%CurrentObject\%\]/gi, guid)
+            : "";
+        const XPath = "//" + this.entity + constraint;
+        mx.data.get({
+            xpath: XPath,
+            filter: {
+                sort: [[this.props.sortAttribute, this.props.sortOrder]],
+                offset: 0,
+                amount: 50
+            },
+            callback: objects => this.processCheckboxItems(objects)
+        });
+    }
+
+    private getDataFromMicroflow = (contextObject?: mendix.lib.MxObject) => {
+        if (contextObject) {
+            if (this.props.dataSourceMicroflow) {
+                this.executeMicroflow(contextObject, this.props.dataSourceMicroflow);
+            } else {
+                mx.ui.error("Error while retrieving checkbox items via microflow, No microflow is specified");
+            }
+        }
+    }
+
+    private processCheckboxItems = (contextObject: mendix.lib.MxObject[]) => {
+        const checkboxItems = contextObject.map(mxObj => {
+            let isChecked = false;
+            const guid = mxObj.getGuid();
+            const caption = mxObj.get(this.props.labelAttribute);
+            const referencedObjects = this.props.mxObject.getReferences(this.reference) as string[];
+            if (referencedObjects !== null && referencedObjects.length > 0) {
+                referencedObjects.map(value => {
+                    if (mxObj.getGuid() === value) {
+                        isChecked = true;
+                    }
+                });
+            }
+
+            return {
+                guid,
+                caption,
+                isChecked
+            };
+        });
+        this.setState({ checkboxItems });
+    }
+
+    private executeMicroflow = (contextObject: mendix.lib.MxObject, microflow: string) => {
+        if (microflow) {
+            window.mx.ui.action(microflow, {
+                callback: (mxObjects: mendix.lib.MxObject[]) => this.processCheckboxItems(mxObjects),
+                error: error => window.mx.ui.error(`Error while executing microflow: ${microflow}: ${error.message}`),
+                params: {
+                    applyto: "selection",
+                    guids: contextObject ? [ contextObject.getGuid() ] : []
+                }
+            });
+        }
     }
 }
